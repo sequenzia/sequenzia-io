331906fc60c0f037701c7b071499fcdbec467b17
Stephen Sequenzia <sequenzia@gmail.com>
2026-01-17 13:15:20 -0500

    init repo

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Sequenzia';
+export const SITE_DESCRIPTION = 'Stephen Sequenzia - ML/AI Engineer';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
